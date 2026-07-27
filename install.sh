e26733e9257d88ac2007808f94994ed5d72b525b
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-07-27 03:47:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,8 @@ sudo pacman -S --needed --noconfirm \
     xclip maim ttf-jetbrains-mono-nerd noto-fonts-emoji \
     gtk3 fastfetch pavucontrol nwg-look mpv brightnessctl xsettingsd micro nano  \
     xorg-xrandr power-profiles-daemon python-gobject arandr polybar \
-    lightdm lightdm-gtk-greeter materia-theme dunst xorg-xinput xsecurelock
+    lightdm lightdm-gtk-greeter materia-theme dunst xorg-xinput xsecurelock \
+    curl jq xdg-utils
 
 # 3. Check and install yay AUR helper
 if ! command -v yay &> /dev/null; then
