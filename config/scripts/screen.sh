a57eeb4a4c66f3a6a9afaddeab31d5885a1d65d3
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-07-28 21:42:05 +0300

    Add files via upload

---
+++ config/scripts/screen.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --mode 1920x1080 --pos 1080x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 0x240 --rotate left --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 3000x240 --rotate right --output DP-3 --off --output DP-4 --primary --mode 1920x1080 --pos 1080x1080 --rotate normal --output DP-5 --off
